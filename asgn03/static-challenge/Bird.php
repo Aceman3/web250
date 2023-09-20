b8014fbb4bb5e4f7b4fd75206bbbe4a1166b11fd
EMatthew <Eric.Matthew@secontainer.com>
2023-09-20 09:09:57 -0400

    Completed birn static challenge

---
+++ asgn03/static-challenge/Bird.php
@@ -0,0 +1,42 @@
+<?php
+
+class Bird {
+    var $habitat;
+    var $food;
+    var $nesting = "tree";
+    var $conservation;
+    var $song = "chirp";
+    var $flying = "yes";
+    static $instance_count = 0;
+    static $egg_num = 0;
+
+    function can_fly() {
+        $flying_string = (static::$flying == "yes") ? "bird can fly" : "cannot fly and is stuck on the ground";
+        return $flying_string;
+    }
+
+
+    public static function create() {
+        return new static();
+    }
+}
+
+class YellowBelliedFlyCatcher extends Bird {
+    var $name = "yellow-bellied flycatcher";
+    var $diet = "mostly insects.";
+    var $song = "flat chilk";
+
+    static $egg_num = "3-4, sometimes 5.";
+}
+
+class Kiwi extends Bird {
+    var $name = "kiwi";
+    var $diet = "omnivorous";
+    var $flying = "no";
+}
+
+$bird = Bird::create();
+echo $bird->can_fly(); 
+
+$kiwi = Kiwi::create();
+echo $kiwi->can_fly(); 
