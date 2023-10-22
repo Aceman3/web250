8b0030b1ed08d6a76b3034ed296e3dd38c0f68e7
EMatthew <Eric.Matthew@secontainer.com>
2023-10-22 17:15:50 -0400

    Completed bird challenge

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Assignments Links</title>
+</head>
+<body>
+    <h1>List of Assignments</h1>
+    <ul>
+        <?php
+        $directory = 'C:/xampp/htdocs/web250'; // Path to the "web250" directory
+        $folders = scandir($directory);
+
+        foreach ($folders as $folder) {
+            $folderPath = $directory . '/' . $folder;
+            if (is_dir($folderPath) && $folder != '.' && $folder != '..' && $folder != '.git' && $folder != '.vs' && $folder !== 'private') {
+                // Generate links for folders (excluding ".", "..", ".git", and "private")
+                echo '<li><a href="' . $folder . '">' . $folder . '</a></li>';
+            }
+        }
+        ?>
+    </ul>
+</body>
+</html>
